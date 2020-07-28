0f9cf61db6de03604bbc05b007ee6fa8987b1f16
Montashevych Serhii <62190126+Slowball@users.noreply.github.com>
2020-07-28 17:55:01 +0300

    last

--- app.js
+++ app.js
@@ -6,19 +6,49 @@ const movieSelect = document.querySelector('#movie');
 
 let ticketPrice = +movieSelect.value;
 
+const setMovieData = (movieIndex, moviePrice) => {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
 const updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     
     const selectedSeatsCount = selectedSeats.length;
 
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
-}
+};
+
+const populateUI = () => {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+    if(selectedSeats !== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected')
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if(selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
+};
+
+populateUI();
 
 movieSelect.addEventListener('change', e => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
-})
+});
 
 container.addEventListener('click', e => {
     if(e.target.classList.contains('seat') && 
@@ -27,4 +57,6 @@ container.addEventListener('click', e => {
 
         updateSelectedCount();
     }
-})
+});
+
+updateSelectedCount();
